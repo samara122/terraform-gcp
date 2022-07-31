c060925af8cc6846366a82ae52ed84689238b0a5
samara <saruufan@gamil.com>
2022-07-31 19:20:32 +0000

    adding project set

--- project_setup.tf
+++ project_setup.tf
@@ -3,7 +3,25 @@ data "google_billing_account" "my_acct" {
 }
 
 resource "random_string" "my_str" {
-  length           = 10
+  length           = 16
   special          = false
   number           = false
+  lower = true
+  upper = false
+}
+
+resource "google_project" "samara_gcp_project1" {
+  name = "team3"
+  project_id = random_string.my_str.id
+  billing_account = data.google_billing_account.my_acct.id
+}
+
+resource "null_resource" "project_set" {
+  triggers = {
+    "always_run" = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+      command = "gcloud config set project ${google_project.samara_gcp_project1.project_id}"
+  }
 }
